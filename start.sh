fc65f1ec7fa7d5b51247c4ae7d0011c3ea65a050
Raffaele <raffaele.mentina@gmail.com>
2023-06-15 21:13:47 +0200

    4 worker

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ python manage.py makemigrations
 python manage.py migrate
 
 python manage.py collectstatic --noinput
-gunicorn PPM_Project.wsgi:application --bind=0.0.0.0:$PORT --timeout 300
+gunicorn PPM_Project.wsgi:application --bind=0.0.0.0:$PORT --timeout 300 --worker 4
